89aea2756872e76b352d54ba331e7d0d53d2a79a
januszmaro <janusz.miro152@gmail.com>
2025-04-25 23:37:16 +0200

    update

--- frontend/src/pages/DocumentView.tsx
+++ frontend/src/pages/DocumentView.tsx
@@ -8,11 +8,11 @@ import { API_URL } from "@/api/axios";
 
 
 const DocumentView = () => {
-    const [text, _] = useState("I am Ubik. Before the universe was, I am. I made the suns. I made the worlds. I created the lives and the places they inhabit; I move them here, I put them there. They go as I say, then do as I tell them. I am the word and my name is never spoken, the name which no one knows. I am called Ubik, but that is not my name. I am. I shall always be.");
+    const [text] = useState("I am Ubik. Before the universe was, I am. I made the suns. I made the worlds. I created the lives and the places they inhabit; I move them here, I put them there. They go as I say, then do as I tell them. I am the word and my name is never spoken, the name which no one knows. I am called Ubik, but that is not my name. I am. I shall always be.");
     const [styleText, setStyleText] = useState('Write in style of Phillip K. Dick');
     const { sendMessage } = useWebSocket(API_URL.replace(/^http/, "ws") + "/api/style-guard", {
     });
-    const [response, setResponse] = useState("");
+    const [response] = useState("");
 
     useEffect(() => {
         const func = async () => {
@@ -20,7 +20,7 @@ const DocumentView = () => {
                 "styleText": styleText
             }));
             console.log(response);
-            setResponse(response.data);
+            // setResponse(response.data);
         }
 
         func();
@@ -32,7 +32,7 @@ const DocumentView = () => {
             "text": text
         }));
         console.log(response);
-        setResponse(response.data);
+        // setResponse(response.data);
     }
 
 
