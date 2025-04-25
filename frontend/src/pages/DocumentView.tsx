225c1d5a2997998bee7ea36c2ce3816a684ba4c0
januszmaro <janusz.miro152@gmail.com>
2025-04-25 23:59:45 +0200

    fix

--- frontend/src/pages/DocumentView.tsx
+++ frontend/src/pages/DocumentView.tsx
@@ -11,7 +11,7 @@ import { toast } from "sonner"
 const DocumentView = () => {
     const [text] = useState("I am Ubik. Before the universe was, I am. I made the suns. I made the worlds. I created the lives and the places they inhabit; I move them here, I put them there. They go as I say, then do as I tell them. I am the word and my name is never spoken, the name which no one knows. I am called Ubik, but that is not my name. I am. I shall always be.");
     const [styleText, setStyleText] = useState('Write in style of Phillip K. Dick');
-    const { sendMessage, lastMessage, readyState } = useWebSocket(API_URL.replace(/^http/, "ws") + "/api/style-guard", {
+    const { sendMessage, lastMessage } = useWebSocket(API_URL.replace(/^http/, "ws") + "/api/style-guard", {
     });
 
     useEffect(() => {
